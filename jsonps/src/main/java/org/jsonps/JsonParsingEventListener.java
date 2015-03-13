95c7a8a36c62d205af564381fe136f1a19db422d
prem <prem.kumar@digitactical.com>
2015-03-14 02:06:58 +0530

    add event listener adapter

---
+++ jsonps/src/main/java/org/jsonps/JsonParsingEventListener.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jsonps;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author prem
+ */
+public class JsonParsingEventListener {
+
+    private static final Logger logger=Logger.getLogger(JsonParsingEventListener.class.getName());
+
+    public void startObject() {
+        logger.log(Level.INFO, "got start object");
+    }
+
+    public void endObject() {
+        logger.log(Level.INFO, "got end object");
+    }
+
+    public void startArray() {
+        logger.log(Level.INFO, "got start array");
+    }
+
+    public void endArray() {
+        logger.log(Level.INFO, "got end array");
+    }
+
+    public void name(String name) {
+        logger.log(Level.INFO, "got name:[{0}]", name);
+    }
+
+    public void value(String value) {
+        logger.log(Level.INFO, "got value:[{0}]", value);
+    }
+}
